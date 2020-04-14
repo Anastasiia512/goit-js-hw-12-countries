c0affc7b398510fa3e5b8af81a9b111609ab9464
Anastasiia <nastyona221985@gmail.com>
2020-04-14 22:27:21 +0300

    PNotify fixing

---
+++ src/clear-content-func.js
@@ -0,0 +1,4 @@
+export default function clearContent() {
+    ulList.textContent = '';
+    divList.textContent = '';
+  }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import PNotify from 'pnotify/dist/es/PNotify';
 import 'pnotify/dist/PNotifyBrightTheme.css';
 import listItemMarkup from './templates/listItem.hbs';
 import listCountryMarkup from './templates/listcountry.hbs';
+import clearContent from './clear-content-func';
 
 const input = document.querySelector('input');
 const ulList = document.querySelector('.list-allcountries');
@@ -14,16 +15,14 @@ const baseURL = 'https://restcountries.eu/rest/v2/name/';
 const handleRequest = function (e) {
   const userInput = input.value;
   if (!userInput) {
-    ulList.textContent = '';
-    divList.textContent = '';
+    clearContent();
   }
-  const responce = axios
+  axios
     .get(`${baseURL}${userInput}`)
     .then(res => {
       console.log(res.data);
       if (res.data.length > 10) {
-        ulList.textContent = '';
-        divList.textContent = '';
+        clearContent();
         PNotify.error({
           title: '!',
           text: 'Too many matches found.Please enter more specific query!',
@@ -31,26 +30,22 @@ const handleRequest = function (e) {
         return;
       }
       if (res.data.length === 1) {
-        divList.textContent = '';
-        ulList.textContent = '';
+        clearContent();
         divList.insertAdjacentHTML('beforeend', listCountryMarkup(res.data[0]));
-        
+
         return;
       }
       if (res.data.length >= 2 && res.data.length <= 10) {
-        ulList.textContent = '';
-        divList.textContent = '';
+        clearContent();
         ulList.insertAdjacentHTML('beforeend', listItemMarkup(res.data));
       }
-    }).catch((err) => {
-        console.log(err.response.data.message);
-        PNotify.error({
-          title: '!',
-          text: err.response.data.message,
-          delay: Infinity,
-        });
+    })
+    .catch(err => {
+      PNotify.error({
+        title: '!',
+        text: err.message,
       });
-      
+    });
 };
 
 input.addEventListener('input', debounce(handleRequest, 500));
